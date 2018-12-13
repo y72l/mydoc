2b3db55e0e548831aafba5d59ca41427918c4b2f
y72l <45660330+y72l@users.noreply.github.com>
2018-12-13 18:50:01 +0800

    Add files via upload

---
+++ dir_cls.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python
+# coding:utf-8
+import os
+class dir_cls:
+       def __init__(self):
+             self.path=os.path.dirname(os.path.realpath(__file__))
+       def get(self,path):
+             return os.listdir(self.path+path)
+       def file(self,path,s):
+             L=[]
+             d=self.get(path)
+             for k in d:
+                 arr=k.split(".")
+                 if len(arr)==2:
+                      if s=="*":
+                         L.append(k)
+                      if arr[1]==s:
+                         L.append(k)
+             return L
+       def dir(self,path):
+             L=[]
+             d=self.get(path)
+             for k in d:
+                 arr=k.split(".")
+                 if len(arr)==1:
+                         L.append(k)
+             return L
---
+++ file_cls.py
@@ -0,0 +1,48 @@
+﻿#!/usr/bin/python
+# coding:utf-8
+import os
+class file_cls:
+       def __init__(self):
+             self.path=os.path.dirname(os.path.realpath(__file__))
+       def read(self,path):
+             with open(self.path+path) as f:
+                  for line in f:
+                       line=line.split(",") 
+                       line=self.filter(line)
+                       yield line
+       def is_number(self,s):
+           try:
+              float(s)
+              return True
+           except ValueError:
+              pass
+           try:
+              import unicodedata
+              unicodedata.numeric(s)
+              return True
+           except (TypeError, ValueError):
+              pass
+           return False
+       def filter(self,L):
+             d=[]
+             for r in L:
+                 r=r.replace("\n","")
+                 r=r.replace("\xef\xbb\xbf","")
+                 r=r.strip()
+                 r=r.lower()
+                 if self.is_number(r):
+                      r=eval(r)
+                 d.append(r)
+             return d
+       def read_all(self,path):
+             d=[]
+             for r in self.read(path):
+                 d.append(r)
+             return d
+       def read_line(self,path,s):
+             d=[]
+             for r in self.read(path):
+                   if s in r:
+                       d=r
+                       break
+             return d
